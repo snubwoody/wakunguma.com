591a14e1ef1d3709942b264fc5822db399a7bb5a
waku <wakunguma13@gmail.com>
2025-04-01 22:00:12 -0600

    config: added tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcs from '@tailwindcss/vite'
+
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite:{
+		plugins:[tailwindcs()]
+	}
+});
