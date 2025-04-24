766bd4ea247a485c83c8485e7ec0e953b81b1aca
waku <wakunguma13@gmail.com>
2025-04-24 10:56:06 -0600

    feat: added storybook

---
+++ svelte-blog/src/stories/Code.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Code from './Code.svelte';
+	import { fn } from '@storybook/test';
+	
+	const { Story } = defineMeta({
+		title: 'Components/Code',
+		component: Code,
+		tags: ['autodocs'],
+	});
+</script>
+
+<Story name="Default"  />
