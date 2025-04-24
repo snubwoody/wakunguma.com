e90bcf88f505e6419855d4b2466a9c43498e158d
waku <wakunguma13@gmail.com>
2025-04-24 12:31:28 -0600

    feat: update `Code` stories

--- svelte-blog/src/stories/Code.stories.svelte
+++ svelte-blog/src/stories/Code.stories.svelte
@@ -1,13 +1,21 @@
 <script module>
 	import { defineMeta } from '@storybook/addon-svelte-csf';
-	import Code from './Code.svelte';
 	import { fn } from '@storybook/test';
 	
 	const { Story } = defineMeta({
 		title: 'Components/Code',
-		component: Code,
 		tags: ['autodocs'],
 	});
 </script>
 
-<Story name="Default"  />
+<Story name="Light">
+	<pre class="code">
+		<code>cargo run --bin app</code>
+	</pre>
+</Story>
+
+<Story name="Dark">
+	<pre data-theme="dark" class="code">
+		<code>cargo run --bin app</code>
+	</pre>
+</Story>
