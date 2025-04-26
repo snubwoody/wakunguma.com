2743dadec54d559e202e4f085fa72124ef84303b
waku <wakunguma13@gmail.com>
2025-04-26 16:20:24 -0600

    feat: new `Navbar` component

---
+++ svelte-blog/src/lib/components/Navbar.svelte
@@ -0,0 +1,4 @@
+
+<nav>
+	<img src="/icons/Logo - Black.svg" alt="Logo" width="24" height="24">
+</nav>
---
+++ svelte-blog/src/stories/Navbar.stories.svelte
@@ -0,0 +1,14 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { fn } from '@storybook/test';
+	import Navbar from '../lib/components/Navbar.svelte'
+	
+	const { Story } = defineMeta({
+		title: 'Components/Navbar',
+		tags: ['autodocs'],
+		component: Navbar
+	});
+</script>
+
+<Story name="Light"/>
+
