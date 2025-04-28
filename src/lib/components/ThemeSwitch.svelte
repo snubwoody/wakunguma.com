00eb3a9604f0e354389e0125f8ba2b569b948172
waku <wakunguma13@gmail.com>
2025-04-28 11:59:24 -0600

    feat: new `ThemeSwitch` component

---
+++ src/lib/components/ThemeSwitch.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import {Sun,Moon} from '@lucide/svelte';
+	import {themeStore} from '$lib/theme.svelte';
+
+	const {switchTheme} = themeStore;
+</script>
+
+<div 
+	aria-label="Theme switcher" 
+	class='theme-switcher'
+>
+	<button 
+		onclick={() => switchTheme('light')} 
+		aria-label="Light mode" 
+		class='light-mode'
+	>
+		<Sun size='20'/>
+	</button>
+	<button 
+		onclick={()=> switchTheme('dark')}
+		aria-label="Dark mode" 
+		class='dark-mode'
+	>
+		<Moon size='20' strokeWidth='2.25'/>
+	</button>
+</div>
+
+<style>
+	.theme-switcher{
+		background-color: var(--color-surface-neutral-dim);
+		display: flex;
+		width: fit-content;
+		gap: 4px;
+		padding: 8px;
+		border-radius: var(--radius-full);
+		align-items: center;
+	}
+
+	button{
+		padding: 8px;
+		border-radius: inherit;
+	}
+
+	.light-mode{
+		position: relative;
+		&::before{
+			position: absolute;
+			content: '';
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			background-color: var(--color-white);
+			border-radius: inherit;
+		}
+
+		
+	}
+
+	svg{
+		position: relative;
+		z-index: 50;
+	}
+</style>
