82b76a524cc322623ed83c8f326c6db74c14fa95
waku <wakunguma13@gmail.com>
2025-04-28 11:02:52 -0600

    feat: add `useTheme` state

---
+++ src/lib/theme.svelte.ts
@@ -0,0 +1,28 @@
+
+
+export type ActiveTheme = 'light' | 'dark';
+
+export function useTheme(){
+	let theme = $state('light');
+
+	let themeVar = localStorage.getItem('theme');
+	if (!themeVar){
+		localStorage.setItem('theme',JSON.stringify('light'));
+		theme = 'light';
+	}else{
+		let activeTheme = JSON.parse(themeVar);
+		theme = activeTheme;
+	}
+
+	return{
+		theme() {
+			return theme;
+		},
+		switchTheme(newTheme: ActiveTheme){
+			localStorage.setItem('theme',JSON.stringify(newTheme));
+			theme = newTheme;
+		}
+	};
+}
+
+export const themeStore = useTheme();
