fd0f09272d653dbe4267382c53787b05e280af79
waku <wakunguma13@gmail.com>
2025-04-28 11:59:47 -0600

    feat: add global theme store to root layout

--- src/lib/theme.svelte.ts
+++ src/lib/theme.svelte.ts
@@ -1,17 +1,21 @@
+import { browser } from "$app/environment";
 
 
 export type ActiveTheme = 'light' | 'dark';
 
 export function useTheme(){
-	let theme = $state('light');
+	let theme: ActiveTheme = $state('light');
 
-	let themeVar = localStorage.getItem('theme');
-	if (!themeVar){
-		localStorage.setItem('theme',JSON.stringify('light'));
-		theme = 'light';
-	}else{
-		let activeTheme = JSON.parse(themeVar);
-		theme = activeTheme;
+	if(browser){
+		let themeVar = localStorage.getItem('theme');
+		if (!themeVar){
+			localStorage.setItem('theme',JSON.stringify('light'));
+			theme = 'light';
+		}
+		else{
+			let activeTheme = JSON.parse(themeVar);
+			theme = activeTheme;
+		}
 	}
 
 	return{
