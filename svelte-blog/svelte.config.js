6675847b1e9c325b6953ea487a692faac4882b0b
waku <wakunguma13@gmail.com>
2025-04-27 18:17:57 -0600

    feat: started implementing the blogs

--- svelte-blog/svelte.config.js
+++ svelte-blog/svelte.config.js
@@ -1,11 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import {mdsvex} from 'mdsvex';
+import {join} from 'path';
 
+const layoutPath = join(import.meta.dirname,'src/lib/components/BlogLayout.svelte')
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.md','.svx']
+	extensions: ['.md','.svx'],
+	layout: layoutPath,
 }
 
 /** @type {import('@sveltejs/kit').Config} */
