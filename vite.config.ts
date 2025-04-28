ded519306a38ae3e4a6199dcf23f604396a2834b
waku <wakunguma13@gmail.com>
2025-04-28 10:21:38 -0600

    refactor: temporarily disable storybook tests

--- vite.config.ts
+++ vite.config.ts
@@ -9,47 +9,51 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
-  test: {
-    workspace: [{
-      extends: './vite.config.ts',
-      plugins: [svelteTesting()],
-      test: {
-        name: 'client',
-        environment: 'jsdom',
-        clearMocks: true,
-        include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-        exclude: ['src/lib/server/**'],
-        setupFiles: ['./vitest-setup-client.ts']
-      }
-    }, {
-      extends: './vite.config.ts',
-      test: {
-        name: 'server',
-        environment: 'node',
-        include: ['src/**/*.{test,spec}.{js,ts}'],
-        exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-      }
-    }, {
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: 'playwright',
-          instances: [{
-            browser: 'chromium'
-          }]
-        },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  }
+	plugins: [tailwindcss(), sveltekit()],
+	test: {
+		workspace: [
+			{
+				extends: './vite.config.ts',
+				plugins: [svelteTesting()],
+				test: {
+					name: 'client',
+					environment: 'jsdom',
+					clearMocks: true,
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts']
+				}
+			}, 
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}, 
+			// {
+			// 	extends: true,
+			// 	plugins: [
+			// 	// The plugin will run tests for the stories defined in your Storybook config
+			// 	// See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+			// 	storybookTest({
+			// 		configDir: path.join(dirname, '.storybook')
+			// 	})],
+			// 	test: {
+			// 		name: 'storybook',
+			// 		browser: {
+			// 		enabled: true,
+			// 		headless: true,
+			// 		provider: 'playwright',
+			// 		instances: [{
+			// 			browser: 'chromium'
+			// 		}]
+			// 		},
+			// 		setupFiles: ['.storybook/vitest.setup.ts']
+			// 	}
+			// }
+		]
+	}
 });
