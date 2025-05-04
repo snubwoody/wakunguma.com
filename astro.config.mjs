851e23b1571c3977e9116cd7462b4b30ba6c9fa3
waku <wakunguma13@gmail.com>
2025-05-03 22:20:05 -0600

    feat: enable prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://wakunguma.com',
+    prefetch: true,
 	integrations: [
         svelte(),
         mdx(), 
