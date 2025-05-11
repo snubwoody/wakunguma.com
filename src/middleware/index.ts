c0092680808d1669b05df556c79de74667bff36e
waku <wakunguma13@gmail.com>
2025-05-11 10:38:11 -0600

    feat: read `theme` cookie in base layout

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,11 +2,9 @@ import { defineMiddleware } from "astro:middleware";
 import { setThemeCookie } from "../lib/theme";
 
 export const onRequest = defineMiddleware((context,next) => {
-    console.log(context.cookies.get("theme"));
     if (!context.cookies.get("theme")){
         setThemeCookie(context.cookies,"light");
     }
-    
 
     return next();
 });
