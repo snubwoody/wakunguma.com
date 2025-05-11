bf8aee5b7affb43530cbf9cdf0098fb4ea84e38c
waku <wakunguma13@gmail.com>
2025-05-11 10:16:49 -0600

    feat: init `theme` cookie in middleware

--- e2e/theme.test.ts
+++ e2e/theme.test.ts
@@ -1,11 +1,76 @@
 import { test, expect } from "@playwright/test";
+import type { ThemeRequest } from "../src/pages/api/theme";
 
-test("Theme switcher", async ({ page }) => {
+test("Theme cookie defaults to light mode", async ({ page,context,browserName }) => {
     await page.goto("http://localhost:4321");
     
-    const response = await fetch("http://localhost:4321/api/theme",{
-        method:"POST"
+    const cookies = await context.cookies();
+    const theme = cookies.find(cookies => cookies.name == "theme");
+    
+    expect(theme?.value).toBe("light");
+    expect(theme?.secure).toBe(true);
+    if (browserName == "webkit"){
+        expect(theme?.sameSite).toBe("None");
+    }else{
+        expect(theme?.sameSite).toBe("Strict");
+    }
+    expect(theme?.httpOnly).toBe(false);
+});
+
+test("Theme endpoint sets cookie", async ({ page,context,browserName }) => {
+    await page.goto("http://localhost:4321");
+
+    const status = await page.evaluate(async () => {
+        const body = {
+            theme: "dark"
+        };
+
+        const response = await fetch("http://localhost:4321/api/theme",{
+            method:"POST",
+            body: JSON.stringify(body),
+            headers: {"content-type":"application/json"}
+        });
+        return response.status;
     });
 
-    console.log(await response.text());
+
+    expect(status).toBe(200);
+
+    const cookies = await context.cookies();
+    const theme = cookies.find(cookies => cookies.name == "theme");
+    
+    expect(theme?.value).toBe("dark");
+    expect(theme?.secure).toBe(true);
+    if (browserName == "webkit"){
+        expect(theme?.sameSite).toBe("None");
+    }else{
+        expect(theme?.sameSite).toBe("Strict");
+    }
+    expect(theme?.httpOnly).toBe(false);
+});
+
+test("Theme cookie persists", async ({ page,context,browserName }) => {
+    await page.goto("http://localhost:4321");
+
+    await page.evaluate(async () => {
+        const body = {
+            theme: "dark"
+        };
+
+        await fetch("http://localhost:4321/api/theme",{
+            method:"POST",
+            body: JSON.stringify(body),
+            headers: {"content-type":"application/json"}
+        });
+    });
+
+
+    const cookies = await context.cookies();
+    const theme1 = cookies.find(cookies => cookies.name == "theme");
+    
+    expect(theme1?.value).toBe("dark");
+    await page.reload();
+    
+    const theme = (await context.cookies()).find(cookie => cookie.name == "theme");
+    expect(theme?.value).toBe("dark");
 });
