b4c041699103b95f6aba08dce31007578b77e9b1
waku <wakunguma13@gmail.com>
2025-05-13 19:26:56 -0600

    feat: add rss endpoint

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,30 @@
+import rss from "@astrojs/rss";
+import { getPosts } from "../lib";
+import type { APIContext } from "astro";
+import fs from "node:fs";
+import path from "node:path";
+
+export async function GET(context: APIContext){
+    const posts = getPosts();
+
+    return rss({
+        title: "Waku's blog",
+        description: "My personal blog",
+        site: context.site ?? "https://wakunguma.com",
+        items: posts.map(post => {
+            const imagePath = path.join("public", post.frontmatter.image);
+            const { size } = fs.statSync(imagePath);
+            return ({
+                title: post.frontmatter.title,
+                description: post.frontmatter.synopsis,
+                pubDate: new Date(post.frontmatter.published),
+                link: post.url,
+                enclosure:{
+                    url: post.frontmatter.image,
+                    length: size,
+                    type: "image/png"
+                }
+            });
+        })
+    });
+}
