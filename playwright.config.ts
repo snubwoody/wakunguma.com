f4d2716127321e11ee014efaae75a5714640c291
waku <wakunguma13@gmail.com>
2025-09-23 08:43:41 -0600

    ci: Run CI pipeline on push to main

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? "50%" : undefined,
-    reporter: [["html", {open:"never"}]],
+    reporter: "line",
     use: {
         baseURL: "http://localhost:4321",
         trace: "on-first-retry"
