9640c1fa1b3f83661525983491783b0c3a48f714
Wakunguma K. <67170475+snubwoody@users.noreply.github.com>
2025-10-06 10:36:58 -0600

    New blog post: Variadic generics (#84)

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,7 +4,6 @@ import { setThemeCookie } from "../lib/theme";
 // TODO: change this to client side so that pages can be prerendered
 export const onRequest = defineMiddleware((context,next) => {
     if (!context.cookies.get("theme")){
-        console.log("Middle ware");
         setThemeCookie(context.cookies,"light");
     }
 
