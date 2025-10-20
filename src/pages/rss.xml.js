1da766489b92dee29aa6bc0238dcc7c036f1189d
waku <wakunguma13@gmail.com>
2025-10-19 19:56:42 -0600

    Fix broken rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,25 +1,26 @@
 import rss from "@astrojs/rss";
 import { getPosts } from "../lib";
 
+export const prerender = true;
+
 // TODO:
 // - Actually check the image size and type
-// - Make this static
-export async function GET(context){
-    const posts = getPosts();
+export async function GET(){
+    const posts = await getPosts();
 
     return rss({
         title: "Waku's blog",
         description: "My personal blog",
-        site: context.site ?? "https://wakunguma.com",
+        site: "https://wakunguma.com",
         items: posts.map(post => {
             return ({
-                title: post.frontmatter.title,
-                description: post.frontmatter.synopsis,
-                pubDate: new Date(post.frontmatter.published),
-                link: post.url,
+                title: post.data.title,
+                description: post.data.synopsis,
+                pubDate: new Date(post.data.published),
+                link: `https://wakunguma.com/blog/${post.id}`,
                 enclosure:{
-                    url: post.frontmatter.image,
-                    length: post.frontmatter.imageSize,
+                    url: `https://wakunguma.com${post.data.image}`,
+                    length: post.data.imageSize,
                     type: "image/png"
                 }
             });
